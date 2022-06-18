d4bb886f3eecc01013a330722708a957dff6378d
dhaou amri <dhaouelamri@gmail.com>
2022-06-18 16:54:26 +0200

    nouveau changement

--- server/api/controllers/magasin/router.js
+++ server/api/controllers/magasin/router.js
@@ -5,5 +5,8 @@ export default express
   .Router()
   .post('/', controller.create)
   .get('/', controller.all)
+  .post('/changenom_magasin', controller.changenom_magasin)
+  .delete('/delete/:id', controller.delete)
+  .post('/changelieu_magasin', controller.changelieu_magasin)
   .get('/byadmin/:id', controller.byadmin)
   .get('/:id', controller.byId);
